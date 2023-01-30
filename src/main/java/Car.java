50381ee32c6c606da564140db7fbaa6698a569cf
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-01-30 10:15:31 +0100

    Add property numberOfTires to car

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,11 @@
 public class Car {
+    private int numberOfTires;
+
+    public void setNumberOfTires(int numberOfTires) {
+        this.numberOfTires = numberOfTires;
+    }
+
+    public int getNumberOfTires() {
+        return numberOfTires;
+    }
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CarTest {
 
     @Test
     void x() {
-        new Car();
+        Car car = new Car();
+        car.setNumberOfTires(4);
+        int actual = car.getNumberOfTires();
+        assertEquals(4, actual);
     }
 }
